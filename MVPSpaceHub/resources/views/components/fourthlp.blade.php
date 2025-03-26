a3c23eded1a39d629203fe9bae9e16b3df30e313
yohanpratamaa <yohanpratamaa@gmail.com>
2025-03-26 21:28:13 +0700

    update

--- MVPSpaceHub/resources/views/components/fourthlp.blade.php
+++ MVPSpaceHub/resources/views/components/fourthlp.blade.php
@@ -7,21 +7,141 @@
     </div>
 
 </div>
-<div class=" w-full h-[1000px] flex justify-center mt-6">
-    <div class="flex justify-center items-center bg-linear-to-b from-[#d2cbfc] to-white w-[1400px] h-[800px] rounded-2xl">
+<div class=" w-full h-[850px] flex justify-center mt-6">
+    <div class="flex justify-center items-center bg-linear-to-b from-[#d2cbfc] to-gray-100 w-[1400px] h-[800px] rounded-2xl">
 
         <div class="grid grid-cols-3 gap-x-10 gap-y-[80px]">
-            <div class="w-[370px] h-[300px] bg-white rounded-2xl shadow-2xl">
+            <div class="w-[370px] h-[300px] bg-white rounded-xl shadow-2xl">
                 <img src="{{asset('images/office.png')}}" alt="" class="w-full">
-                <div class="category flex">
-                    <div class="">Coworking space</div>
+                <div class="category flex text-white">
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-green-400 rounded-xl text-[12px]">Coworking space</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-red-400 rounded-xl text-[12px]">08.00 - 22.00</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-yellow-400 rounded-xl text-[12px]">3 Ruangan</div>
+                </div>
+
+                <div class="flex w-full relative mt-2">
+                    <h1 class="mx-2 mt-1 text-lg font-semibold tracking-wide text-gray-800">Virtual Office(vOffice)</h1>
+                    <h1 class="absolute text-lg font-semibold right-0 mx-2 mt-1 tracking-wide">Rp. 85.000</h1>
+                </div>
+
+                <div class="flex w-full relative">
+                    <h1 class="px-2 text-[12px] tracking-wide text-gray-500">Jl. Braga No. 109, Bandung</h1>
+                    <h1 class="absolute right-0 px-2 text-gray-500">/Ruangan</h1>
                 </div>
 
 
             </div>
 
-        </div>
+            <div class="w-[370px] h-[300px] bg-white rounded-xl shadow-2xl">
+                <img src="{{asset('images/office.png')}}" alt="" class="w-full">
+                <div class="category flex text-white">
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-green-400 rounded-xl text-[12px]">Coworking space</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-red-400 rounded-xl text-[12px]">08.00 - 22.00</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-yellow-400 rounded-xl text-[12px]">3 Ruangan</div>
+                </div>
 
+                <div class="flex w-full relative mt-2">
+                    <h1 class="mx-2 mt-1 text-lg font-semibold tracking-wide text-gray-800">Virtual Office(vOffice)</h1>
+                    <h1 class="absolute text-lg font-semibold right-0 mx-2 mt-1 tracking-wide">Rp. 85.000</h1>
+                </div>
+
+                <div class="flex w-full relative">
+                    <h1 class="px-2 text-[12px] tracking-wide text-gray-500">Jl. Braga No. 109, Bandung</h1>
+                    <h1 class="absolute right-0 px-2 text-gray-500">/Ruangan</h1>
+                </div>
+
+
+            </div>
+
+            <div class="w-[370px] h-[300px] bg-white rounded-xl shadow-2xl">
+                <img src="{{asset('images/office.png')}}" alt="" class="w-full">
+                <div class="category flex text-white">
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-green-400 rounded-xl text-[12px]">Coworking space</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-red-400 rounded-xl text-[12px]">08.00 - 22.00</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-yellow-400 rounded-xl text-[12px]">3 Ruangan</div>
+                </div>
+
+                <div class="flex w-full relative mt-2">
+                    <h1 class="mx-2 mt-1 text-lg font-semibold tracking-wide text-gray-800">Virtual Office(vOffice)</h1>
+                    <h1 class="absolute text-lg font-semibold right-0 mx-2 mt-1 tracking-wide">Rp. 85.000</h1>
+                </div>
+
+                <div class="flex w-full relative">
+                    <h1 class="px-2 text-[12px] tracking-wide text-gray-500">Jl. Braga No. 109, Bandung</h1>
+                    <h1 class="absolute right-0 px-2 text-gray-500">/Ruangan</h1>
+                </div>
+
+
+            </div>
+
+            <div class="w-[370px] h-[300px] bg-white rounded-xl shadow-2xl">
+                <img src="{{asset('images/office.png')}}" alt="" class="w-full">
+                <div class="category flex text-white">
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-green-400 rounded-xl text-[12px]">Coworking space</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-red-400 rounded-xl text-[12px]">08.00 - 22.00</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-yellow-400 rounded-xl text-[12px]">3 Ruangan</div>
+                </div>
+
+                <div class="flex w-full relative mt-2">
+                    <h1 class="mx-2 mt-1 text-lg font-semibold tracking-wide text-gray-800">Virtual Office(vOffice)</h1>
+                    <h1 class="absolute text-lg font-semibold right-0 mx-2 mt-1 tracking-wide">Rp. 85.000</h1>
+                </div>
+
+                <div class="flex w-full relative">
+                    <h1 class="px-2 text-[12px] tracking-wide text-gray-500">Jl. Braga No. 109, Bandung</h1>
+                    <h1 class="absolute right-0 px-2 text-gray-500">/Ruangan</h1>
+                </div>
+
+
+            </div>
+
+            <div class="w-[370px] h-[300px] bg-white rounded-xl shadow-2xl">
+                <img src="{{asset('images/office.png')}}" alt="" class="w-full">
+                <div class="category flex text-white">
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-green-400 rounded-xl text-[12px]">Coworking space</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-red-400 rounded-xl text-[12px]">08.00 - 22.00</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-yellow-400 rounded-xl text-[12px]">3 Ruangan</div>
+                </div>
+
+                <div class="flex w-full relative mt-2">
+                    <h1 class="mx-2 mt-1 text-lg font-semibold tracking-wide text-gray-800">Virtual Office(vOffice)</h1>
+                    <h1 class="absolute text-lg font-semibold right-0 mx-2 mt-1 tracking-wide">Rp. 85.000</h1>
+                </div>
+
+                <div class="flex w-full relative">
+                    <h1 class="px-2 text-[12px] tracking-wide text-gray-500">Jl. Braga No. 109, Bandung</h1>
+                    <h1 class="absolute right-0 px-2 text-gray-500">/Ruangan</h1>
+                </div>
+
+
+            </div>
+
+            <div class="w-[370px] h-[300px] bg-white rounded-xl shadow-2xl">
+                <img src="{{asset('images/office.png')}}" alt="" class="w-full">
+                <div class="category flex text-white">
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-green-400 rounded-xl text-[12px]">Coworking space</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-red-400 rounded-xl text-[12px]">08.00 - 22.00</div>
+                    <div class="p-1 h-[25px] ml-3 mt-2 bg-yellow-400 rounded-xl text-[12px]">3 Ruangan</div>
+                </div>
+
+                <div class="flex w-full relative mt-2">
+                    <h1 class="mx-2 mt-1 text-lg font-semibold tracking-wide text-gray-800">Virtual Office(vOffice)</h1>
+                    <h1 class="absolute text-lg font-semibold right-0 mx-2 mt-1 tracking-wide">Rp. 85.000</h1>
+                </div>
+
+                <div class="flex w-full relative">
+                    <h1 class="px-2 text-[12px] tracking-wide text-gray-500">Jl. Braga No. 109, Bandung</h1>
+                    <h1 class="absolute right-0 px-2 text-gray-500">/Ruangan</h1>
+                </div>
+
+
+            </div>
+
+
+        </div>
+        <div class="p-3 px-4 mx-[60px] text-white rounded-lg bg-[#2F327D] bottom-0 right-0 absolute">
+            Lebih Banyak
+        </div>
 
     </div>
 
