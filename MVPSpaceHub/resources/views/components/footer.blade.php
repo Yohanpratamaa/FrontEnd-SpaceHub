3e82c2fa337ca198424286e2d9a87d222bdbac8c
yohanpratamaa <yohanpratamaa@gmail.com>
2025-03-27 10:26:10 +0700

    update footer

--- MVPSpaceHub/resources/views/components/footer.blade.php
+++ MVPSpaceHub/resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full h-[420px] flex justify-center items-center">
 
-    <div class="grid grid-cols-4">
+    <div class="grid grid-cols-4 relative">
 
         <div class="ml-[100px]">
             <img src="{{ asset('images/logo.png') }}" alt="" class="w-[150px]">
@@ -8,7 +8,7 @@
 
         <div class="w-[280px] tracking-wide">
             <h1 class="text-gray-900 text-wrap text-xl">Bandung Techno Park (BTP), Telkom University Bandung</h1>
-            <p class="text-sm text-gray-500">Jl. Telekomunikasi No. 1, Bandung, Jawa Barat</p>
+            <p class="text-sm text-gray-500 mt-2">Jl. Telekomunikasi No. 1, Bandung, Jawa Barat</p>
             <div class="mt-[80px]">
                 <p class="mb-3">(434) 546-4356</p>
                 <p>hspace64@gmail.com</p>
@@ -22,7 +22,7 @@
                 <li class="mb-3">Fitur</li>
                 <li class="mb-3">Explore</li>
                 <li class="mb-3">Kerja Sama</li>
-                <li class="mt-[70px]">© 2020 Lift Media. All rights reserved.</li>
+                <li class="mt-[75px]">© 2020 Lift Media. All rights reserved.</li>
             </ul>
         </div>
 
@@ -32,6 +32,11 @@
                 <li class="mb-3">Linkedln</li>
                 <li>Instagram</li>
             </ul>
+
+        </div>
+
+        <div class="absolute right-0 mr-[50px]">
+            <img src="{{ asset('images/UpArrow.png') }}" alt="">
 
         </div>
     </div>
